685740de9855c7d02018d158952db01c88b1bcaf
Pushpendra Dwivedi <mhanu6815@gmail.com>
2024-08-23 14:24:00 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv"
 
 import DBconnect from "./db/index.js"
+import { app } from "./app.js"
 dotenv.config({
     path:'./env'
 })
 
 
-DBconnect();
+DBconnect()
+.then(()=>{
+    app.listen(process.env.PORT || 3000 , ()=>{
+        console.log(`server running on port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection failed" , err)
+})
  
