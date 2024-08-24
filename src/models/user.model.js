2c452a7f52bbb98127b59d7d3e8d235a9d5dee9c
Pushpendra Dwivedi <mhanu6815@gmail.com>
2024-08-24 15:03:44 +0530

    controllers and routes setup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -76,16 +76,15 @@ userSchema.methods.generateAccessToken = function () {
   );
 };
 userSchema.methods.generateRefreshToken = function () {
-    return jwt.sign(
-      {
-        _id: this._id,
-       
-      },
-      process.env.REFRESH_TOKEN_SECRET,
-      {
-        expiresIn: proccess.env.REFRESH_TOKEN_EXPIRY,
-      }
-    );
-  };
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: proccess.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as coudinary } from "cloudinary";
+
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null;
+       const response=await cloudinary.uploader.upload(localFilePath , {
+            resource_type: "auto"
+        })
+        // the file is uploaded successfullly
+        console.log("file is uploadd succesfully", response.url);
+        return response;
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // remove the locally save dtemporary file as upload got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
